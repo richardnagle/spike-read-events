b0eded1afaf8fc5c43d7a7d2dea877cb7cb96c75
Richard Nagle <richard.nagle@firsthelpline.com>
2017-03-14 16:26:32 +0000

    add EventReceiver project

---
+++ src/EventReceiver/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Fri.Xhl.Domain.Events;
+using MassTransit;
+
+namespace EventReceiver
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var bus = MassTransit.Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                var host = cfg.Host(new Uri("rabbitmq://localhost/"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+
+                cfg.ReceiveEndpoint(host, "event_receiver_queue", e =>
+                {
+                    e.Consumer<EventConsumer>();
+                });
+
+            });
+
+            bus.Start();
+        }
+    }
+
+    public class EventConsumer: IConsumer<Message>
+    {
+        public async Task Consume(ConsumeContext<Message> context)
+        {
+            var message = context.Message;
+            await Console.Out.WriteLineAsync($"{DateTime.Now.ToLongTimeString()} Received event {message.GetType()}");
+
+        }
+    }
+}
